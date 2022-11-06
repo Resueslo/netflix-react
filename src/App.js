8c80c0af6fa675e276201230b0a12befdc44ab60
Reyna Escobar <reyna.escobar94@gmail.com>
2022-11-06 11:10:09 -0700

    Se cambia el manejo de router a como se vio en clase

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import './App.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-//VISTAS
-import Error from './views/Error';
-import Detalle from './views/Detalle';
-import Home from './views/Home.js'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/home",
-    element: <Home />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/detalle",
-    element: <Detalle />,
-    errorElement: <Error />,
-  },
-]);
 
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <h1>app</h1>
     </>
   );
 }
